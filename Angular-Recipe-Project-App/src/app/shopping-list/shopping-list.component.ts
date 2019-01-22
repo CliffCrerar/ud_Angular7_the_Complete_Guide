b1067a2937275a0593212fd0e9eb1ee7681af891
Cliffenator <cliff.crerar@gmail.com>
2019-01-23 01:56:18 +0200

    Creating ingredients

---
+++ Angular-Recipe-Project-App/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
+
+@Component({
+	selector: 'app-shopping-list',
+	templateUrl: './shopping-list.component.html',
+	styleUrls: [ './shopping-list.component.css' ]
+})
+export class ShoppingListComponent implements OnInit {
+	ingredients = [ new Ingredients('Apples', 5), new Ingredients('Tomatos', 10) ];
+
+	constructor() {}
+
+	ngOnInit() {}
+}
