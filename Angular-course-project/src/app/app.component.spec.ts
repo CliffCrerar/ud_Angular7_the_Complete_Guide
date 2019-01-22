a2ac6e98cb2d5d648e8d66fa3761c40ac4f871fe
Cliffenator <cliff.crerar@gmail.com>
2019-01-22 02:10:22 +0200

    Project messed up

--- Angular-course-project/src/app/app.component.spec.ts
+++ Angular-course-project/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-angular-app'`, () => {
+  it(`should have as title 'Angular-course-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('Angular-course-project');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-angular-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-course-project!');
   });
 });
