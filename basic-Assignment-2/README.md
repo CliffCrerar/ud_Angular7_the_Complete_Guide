232caccb10dc0418b14e565129fd85f7126d6ea3
Cliffenator <cliff.crerar@gmail.com>
2019-01-22 04:29:52 +0200

    Created assignment 2 app

--- basic-Assignment-2/README.md
+++ basic-Assignment-2/README.md
@@ -1,4 +1,4 @@
-# AngularCourseProject
+# BasicAssignment2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.6.
 
--- basic-Assignment-2/src/app/app.component.spec.ts
+++ basic-Assignment-2/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-angular-app'`, () => {
+  it(`should have as title 'basicAssignment2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).toEqual('basicAssignment2');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-angular-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to basicAssignment2!');
   });
 });
