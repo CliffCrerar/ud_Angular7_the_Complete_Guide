a0cc6ff19955111701cdc8050216075b20db110d
Cliffenator <cliff.crerar@gmail.com>
2019-01-22 23:58:39 +0200

    eya

--- Angular-Recipe-Project-App/src/app/app.component.ts
+++ Angular-Recipe-Project-App/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularRecipeProjectApp';
+  title = 'Angular-Recipe-Project-App';
 }
