54cff12adf34d4e80263688e7ca8e548fe14ac12
Cliffenator <cliff.crerar@gmail.com>
2019-01-27 22:44:38 +0200

    making changes

--- 5_cmp-databindings/src/app/app.component.ts
+++ 5_cmp-databindings/src/app/app.component.ts
@@ -3,14 +3,18 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: [
+		'./app.component.css',
+	],
 })
 export class AppComponent {
-	serverElements = [{type: 'server',name: 'Testserver', content: 'Just a test!'}];
+	serverElements = [
+		{ type: 'server', name: 'Testserver', content: 'Just a test!' },
+	];
 	// newServerName;
 	// newServerContent;
 
-	onServerAdded(serverData: {serverName: string, serverContent: string}) {
+	onServerAdded (serverData: { serverName: string; serverContent: string }) {
 		console.log('serverData: ', serverData);
 		this.serverElements.push({
 			type: 'server',
@@ -18,12 +22,12 @@ export class AppComponent {
 			content: serverData.serverContent,
 		});
 	}
-	onBluePrintAdded(bluePrintData: {serverName: string, serverContent: string}) {
+	onBluePrintAdded (bluePrintData: { serverName: string; serverContent: string }) {
 		console.log('bluePrintData: ', bluePrintData);
 		this.serverElements.push({
 			type: 'blueprint',
 			name: bluePrintData.serverName,
-			content: bluePrintData.serverContent
+			content: bluePrintData.serverContent,
 		});
 	}
 }
