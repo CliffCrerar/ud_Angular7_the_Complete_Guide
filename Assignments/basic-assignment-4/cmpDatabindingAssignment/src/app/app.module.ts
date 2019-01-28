ff8b07b4f740725993e89bfda9bf012a1a1378a5
CliffCrerar <cliff.crerar@gmail.com>
2019-01-28 19:24:52 +0200

    audit shit babay

--- Assignments/basic-assignment-4/cmpDatabindingAssignment/src/app/app.module.ts
+++ Assignments/basic-assignment-4/cmpDatabindingAssignment/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule( {
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
