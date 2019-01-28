cbad835eff155af4a833dc7f16658e3ffe918ea6
CliffCrerar <cliffordc@acl.co.za>
2019-01-28 11:30:42 +0200

    Doing shit

--- 2_recipes_app/src/app/header/header.component.ts
+++ 2_recipes_app/src/app/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-	selector: 'app-header',
-	templateUrl: './header.component.html',
-	styleUrls: [
-		'./header.component.css',
-	],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  @Output() featureSelected = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit() {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+  }
+
+  onSelect(feature: string) {
+    // console.log('feature: ', feature);
+    this.featureSelected.emit(feature);
+  }
+}
--- 2_recipes_app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 2_recipes_app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -15,9 +16,14 @@ export class RecipeListComponent implements OnInit {
 			'this is a test recipe fucker',
 			'https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg',
 		),
+		new Recipe(
+			'Test recipe',
+			'this is a test recipe fucker',
+			'https://media2.s-nbcnews.com/j/newscms/2018_35/1363730/rachel-hollis-chicken-fingers-today-main-180828_b9b2a726ec8654e3f9f7435ce26588fb.today-inline-large.jpg',
+		),
 	];
 
-	constructor() {}
+	constructor () {}
 
-	ngOnInit() {}
+	ngOnInit () {}
 }
