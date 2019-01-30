8f7f9829a9f9b4d679df61a40a088009603fbedc
CliffCrerar <cliff.crerar@gmail.com>
2019-01-30 08:20:50 +0200

    Yeah

--- 9_services-start/src/app/app.component.ts
+++ 9_services-start/src/app/app.component.ts
@@ -1,31 +1,19 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
-
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
-
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './accounts.service';
+@Component( {
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ],
+	providers: [ AccountService ]
+} )
+export class AppComponent implements OnInit {
+	accounts: { name: string, status: string }[] = [];
+	constructor ( private accountsService: AccountService ) {
+		console.log( 'accountsService: ', accountsService );
+		console.log( 'this.accounts: ', this.accounts );
+	}
+	ngOnInit () {
+		this.accounts = this.accountsService.accounts;
+		console.log( 'this.accounts: ', this.accounts );
+	}
 }
