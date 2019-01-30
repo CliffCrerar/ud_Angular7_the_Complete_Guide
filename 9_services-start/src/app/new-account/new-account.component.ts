54e875b2de194b8f2a5525bcab33b33b8c8c5aa2
CliffCrerar <cliff.crerar@gmail.com>
2019-01-30 10:48:06 +0200

    asdas

--- 9_services-start/src/app/new-account/new-account.component.ts
+++ 9_services-start/src/app/new-account/new-account.component.ts
@@ -5,13 +5,15 @@ import { AccountService } from '../accounts.service';
 	selector: 'app-new-account',
 	templateUrl: './new-account.component.html',
 	styleUrls: [ './new-account.component.css' ],
-	providers: [ LoggingService, AccountService ]
+	providers: [ LoggingService ]
 } )
 export class NewAccountComponent {
-	constructor ( private loggingService: LoggingService, private accountService: AccountService ) { }
+	constructor ( private loggingService: LoggingService, private accountService: AccountService ) {
+		console.log( 'AccountService 1: ', accountService );
+	}
 	onCreateAccount ( accountName: string, accountStatus: string ) {
 		this.accountService.addAccount( accountName, accountStatus );
 		this.loggingService.logStatusChange( accountStatus );
+		console.log( 'NEW ACCOUNTS 2: ', this.accountService );
 	}
 }
-
